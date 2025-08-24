c1a76717d9f798fd04e7aabd2bc77127fff9a272
Harry Muriithi <hharryjjack@gmail.com>
2025-08-24 23:56:56 +0300

    popup.tsx fix

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -30,7 +30,7 @@ const Popup: React.FC<PopupProps> = ({ onClose }) => {
           playsInline
           className="w-full rounded-lg"
         >
-          <source src="/promo 2.mp4" type="video/mp4" />
+          <source src="/promo.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
